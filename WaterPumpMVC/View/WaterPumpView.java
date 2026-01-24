35eba4a065c0bcdc4ee3f3ab7322776d74b69405
MarlonTacadena <158710284+MarlonTacadena@users.noreply.github.com>
2026-01-24 18:33:35 +0800

    WaterPumpView.java

--- WaterPumpMVC/View/WaterPumpView.java
+++ WaterPumpMVC/View/WaterPumpView.java
@@ -1,1 +1,21 @@
+package View;
+
+import java.util.Scanner;
+
+public class WaterPumpView {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getWaterLevelInput() {
+        System.out.print("Enter water level (0-100): ");
+        return scanner.nextInt();
+    }
+
+    public void displayPumpStatus(boolean pumpOn) {
+        if (pumpOn) {
+            System.out.println("Pump is ON.");
+        } else {
+            System.out.println("Pump is OFF.");
+        }
+    }
+}
 
