b0994da1f5b1cf8ed25edc78dba66c31d0b09e26
MarlonTacadena <158710284+MarlonTacadena@users.noreply.github.com>
2026-01-24 18:34:24 +0800

    WaterPumpController.java

--- WaterPumpMVC/Controller/WaterPumpController.java
+++ WaterPumpMVC/Controller/WaterPumpController.java
@@ -1,1 +1,21 @@
+package Controller;
+
+import Model.WaterPumpModel;
+import View.WaterPumpView;
+
+public class WaterPumpController {
+    private WaterPumpModel model;
+    private WaterPumpView view;
+
+    public WaterPumpController(WaterPumpModel model, WaterPumpView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() {
+        int waterLevel = view.getWaterLevelInput();
+        model.setWaterLevel(waterLevel);
+        view.displayPumpStatus(model.isPumpOn());
+    }
+}
 
