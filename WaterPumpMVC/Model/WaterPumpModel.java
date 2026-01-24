e69b44c76ea74d92d85cc12cb60023cecc4c8b52
MarlonTacadena <158710284+MarlonTacadena@users.noreply.github.com>
2026-01-24 18:31:17 +0800

    WaterPumpModel.java

--- WaterPumpMVC/Model/WaterPumpModel.java
+++ WaterPumpMVC/Model/WaterPumpModel.java
@@ -1,1 +1,28 @@
 
+package Model;
+
+public class WaterPumpModel {
+    private int waterLevel;
+    private boolean pumpOn;
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int waterLevel) {
+        this.waterLevel = waterLevel;
+        updatePumpStatus();
+    }
+
+    public boolean isPumpOn() {
+        return pumpOn;
+    }
+
+    private void updatePumpStatus() {
+        if (waterLevel < 20) {
+            pumpOn = true;
+        } else if (waterLevel > 80) {
+            pumpOn = false;
+        }
+    }
+}
