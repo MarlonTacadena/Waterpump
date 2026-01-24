a40f97502395ae3774046511d8cc6a94bc6938f2
MarlonTacadena <158710284+MarlonTacadena@users.noreply.github.com>
2026-01-24 18:35:05 +0800

    Main.java

--- WaterPumpMVC/Main.java
+++ WaterPumpMVC/Main.java
@@ -1,1 +1,14 @@
+import Model.WaterPumpModel;
+import View.WaterPumpView;
+import Controller.WaterPumpController;
+
+public class Main {
+    public static void main(String[] args) {
+        WaterPumpModel model = new WaterPumpModel();
+        WaterPumpView view = new WaterPumpView();
+        WaterPumpController controller = new WaterPumpController(model, view);
+
+        controller.run();
+    }
+}
 
